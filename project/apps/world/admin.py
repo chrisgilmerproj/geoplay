e20325ade104ad4573cae44b978c573ee8c6766c
Chris Gilmer <chris.gilmer@gmail.com>
2011-05-24 15:31:58 -0700

    Added google maps to world app admin

--- project/apps/world/admin.py
+++ project/apps/world/admin.py
@@ -1,5 +1,14 @@
+from django.conf import settings
 from django.contrib.gis import admin
+from django.contrib.gis.maps.google import GoogleMap
+
 from world.models import WorldBorders
 
-#admin.site.register(WorldBorders, admin.GeoModelAdmin)
-admin.site.register(WorldBorders, admin.OSMGeoAdmin)
+GMAP = GoogleMap(key=settings.GOOGLE_MAPS_API_KEY)
+
+class WorldBordersAdmin(admin.OSMGeoAdmin):
+	extra_js     = [GMAP.api_url + GMAP.key]
+	map_template = 'gis/admin/google.html'
+	modifiable   = True
+
+admin.site.register(WorldBorders, WorldBordersAdmin)
